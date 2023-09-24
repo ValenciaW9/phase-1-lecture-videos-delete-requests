e3dee64b6255d4c5532981cd3901b7543be3b92c
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-24 16:04:57 -0400

    phase-1-lecture-videos-delete-requests/
    phase-1-lecture-videos-delete-requests/

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function submitData(name, email) {
   return fetch('http://localhost:3000/users', {
+    method: 'POST', // Add the method property to specify it as a POST request
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
@@ -14,9 +15,11 @@ function submitData(name, email) {
   })
   .then(function(data) {
     document.body.innerHTML += `<p>User ID: ${data.id}</p>`;
+    return data; // Return the data for the next `then` block
   })
   .catch(function(error) {
     document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    throw error; // Rethrow the error for the next `catch` block
   });
 }
 
