eca9344741993e5f02f32c671653954e059355bb
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-24 17:26:23 -0400

    phase-1-lecture-videos-delete-requests/
    phase-1-lecture-videos-delete-requests/

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-import submitData from '../src/index';
-function submitData(name, email) {
-  return fetch('http://localhost:3000/users', {
-    method: 'POST', // Add the method property to specify it as a POST request
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-      name: name,
-      email: email
-    })
-  })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    document.body.innerHTML += `<p>User ID: ${data.id}</p>`;
-    return data; // Return the data for the next `then` block
-  })
-  .catch(function(error) {
-    document.body.innerHTML += `<p>Error: ${error.message}</p>`;
-    throw error; // Rethrow the error for the next `catch` block
-  });
-}
+const userData = {
+  name: name,
+  email: email
+};
 
-export default submitData;
+const jsonData = JSON.stringify(userData);
 
+const headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json'
+};
+
+fetch('http://localhost:3000/users', {
+  method: 'POST',
+  headers: headers,
+  body: jsonData
+})
+.then(response => {
+  // Handle the response
+})
+.then(data => {
+  // Access the converted object
+})
+.then(data => {
+  const id = data.id;
+  // Append the id to the DOM
+})
+.catch(error => {
+  // Handle the error
+})
+.catch(error => {
+  const errorMessage = error.message;
+  // Append the errorMessage to the DOM
+})
+return fetch('http://localhost:3000/users', {
+  method: 'POST',
+  headers: headers,
+  body: jsonData
+})
