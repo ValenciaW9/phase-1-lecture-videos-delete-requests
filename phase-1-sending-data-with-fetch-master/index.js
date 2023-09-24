7dd73fbb34317f7639af57e092017a0dff94301d
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-24 15:48:03 -0400

    phase-1-lecture-videos-delete-requests/
    phase-1-lecture-videos-delete-requests/

--- phase-1-sending-data-with-fetch-master/index.js
+++ phase-1-sending-data-with-fetch-master/index.js
@@ -1,52 +1,21 @@
 function submitData(name, email) {
-  const url = "http://localhost:3000/users";
-  const headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  };
-  const body = JSON.stringify({
-    name: name,
-    email: email
-  });
-
-  return fetch(url, {
-    method: "POST",
-    headers: headers,
-    body: body
+  return fetch('http://localhost:3000/users', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
   })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      // TODO: Find the new id in the data and append it to the DOM
-    })
-    .catch(function(error) {
-      // TODO: Append the error message to the DOM
-    });
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    document.body.innerHTML += `<p>User ID: ${data.id}</p>`;
+  })
+  .catch(function(error) {
+    document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+  });
 }
-
-module.exports = {
-  submitData
-};
-Make sure that the submitData function is exported using module.exports.
-
-Now, in your Mocha test file indexTest.js, you can import the submitData function and write the tests:
-
-const { submitData } = require("../index.js");
-
-describe("submitData()", function() {
-  // Test 1
-  it("makes a POST request to /users with a name and email", function() {
-    // TODO: Write the test
-  });
-
-  // Test 2
-  it("handles a failed POST request using catch, appends the error message to the DOM", function() {
-    // TODO: Write the test
-  });
-
-  // Test 3
-  it("handles the POST request response, retrieves the new id value and appends it to the DOM", function() {
-    // TODO: Write the test
-  });
-});
