7822a2ea559869011f2b7b3eded334d4003ceb25
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-24 15:44:09 -0400

    phase-1-lecture-videos-delete-requests/
    phase-1-lecture-videos-delete-requests/

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const url = "http://localhost:3000/users";
-  const headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  };
-  const body = JSON.stringify({
-    name: name,
-    email: email
-  });
-
-  // TODO: Write the fetch() request
-
-}
-
 function submitData(name, email) {
   const url = "http://localhost:3000/users";
   const headers = {
@@ -27,24 +13,6 @@ function submitData(name, email) {
     method: "POST",
     headers: headers,
     body: body
-  });
-}
-
-unction submitData(name, email) {
-  const url = "http://localhost:3000/users";
-  const headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  };
-  const body = JSON.stringify({
-    name: name,
-    email: email
-  });
-
-  return fetch(url, {
-    method: "POST",
-    headers: headers,
-    body: body
   })
     .then(function(response) {
       return response.json();
@@ -57,29 +25,28 @@ unction submitData(name, email) {
     });
 }
 
-unction submitData(name, email) {
-  const url = "http://localhost:3000/users";
-  const headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  };
-  const body = JSON.stringify({
-    name: name,
-    email: email
+module.exports = {
+  submitData
+};
+Make sure that the submitData function is exported using module.exports.
+
+Now, in your Mocha test file indexTest.js, you can import the submitData function and write the tests:
+
+const { submitData } = require("../index.js");
+
+describe("submitData()", function() {
+  // Test 1
+  it("makes a POST request to /users with a name and email", function() {
+    // TODO: Write the test
   });
 
-  return fetch(url, {
-    method: "POST",
-    headers: headers,
-    body: body
-  })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      // TODO: Find the new id in the data and append it to the DOM
-    })
-    .catch(function(error) {
-      // TODO: Append the error message to the DOM
-    });
-}
+  // Test 2
+  it("handles a failed POST request using catch, appends the error message to the DOM", function() {
+    // TODO: Write the test
+  });
+
+  // Test 3
+  it("handles the POST request response, retrieves the new id value and appends it to the DOM", function() {
+    // TODO: Write the test
+  });
+});
