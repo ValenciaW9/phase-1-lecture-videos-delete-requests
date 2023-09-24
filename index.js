fa130cd83a3a63f24bb62469494479f3ebb84575
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-24 18:46:32 -0400

    phase-1-lecture-videos-delete-requests/
    phase-1-lecture-videos-delete-requests/

--- index.js
+++ index.js
@@ -24,4 +24,60 @@ function submitData(name, email) {
     });
 }
 
+// Test 1
+fetch('http://localhost:3000/users')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Process the data here
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// Test 2
+function handleResponse(response) {
+  if (!response.ok) {
+    throw new Error('Network response was not OK');
+  }
+  return response.json();
+}
+
+fetch('http://localhost:3000/users')
+  .then(handleResponse)
+  .then(data => {
+    console.log(data); // Process the data here
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// Test 3
+async function fetchData() {
+  try {
+    const response = await fetch('http://localhost:3000/users');
+    const data = await response.json();
+    console.log(data); // Process the data here
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
+
+// Test 4
+fetch('http://localhost:3000/users')
+  .then(response => response.json())
+  .then(data => {
+    // Process the data here
+    const userList = document.getElementById("user-list");
+    data.forEach(user => {
+      const listItem = document.createElement("li");
+      listItem.textContent = user.name;
+      userList.appendChild(listItem);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
 // Rest of the code...
