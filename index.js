f7775f1a6995ed7f2de292eb1a3c4589df7db22e
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-24 18:29:02 -0400

    phase-1-lecture-videos-delete-requests/
    phase-1-lecture-videos-delete-requests/

--- index.js
+++ index.js
@@ -22,6 +22,56 @@ function submitData(name, email) {
     });
 }
 
-module.exports = {
-  submitData
-};
+function submitData() {
+  let formData = {
+    // your form data here
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  fetch("http://example.com/api/endpoint", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let newId = data.id;
+      // Append the newId to the DOM
+      document.getElementById("idContainer").innerText = newId;
+    });
+}
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // Process the data here
+  })
+  .catch(error => {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = error.message;
+    document.getElementById('error-container').appendChild(errorMessage);
+  });
+  function submitData(url, data) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(json => {
+        // Handle the response data
+        console.log(json);
+      })
+      .catch(error => {
+        // Handle any errors
+        console.log(error);
+      });
+  }
