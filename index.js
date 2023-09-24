56b69d867b2cedda024a29eac1fdf03c31117765
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-24 17:29:49 -0400

    phase-1-lecture-videos-delete-requests/
    phase-1-lecture-videos-delete-requests/

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-const userData = {
-  name: name,
-  email: email
-};
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
 
-const jsonData = JSON.stringify(userData);
+  const jsonData = JSON.stringify(userData);
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json'
-};
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
 
-fetch('http://localhost:3000/users', {
-  method: 'POST',
-  headers: headers,
-  body: jsonData
-})
-.then(response => {
-  // Handle the response
-})
-.then(data => {
-  // Access the converted object
-})
-.then(data => {
-  const id = data.id;
-  // Append the id to the DOM
-})
-.catch(error => {
-  // Handle the error
-})
-.catch(error => {
-  const errorMessage = error.message;
-  // Append the errorMessage to the DOM
-})
-return fetch('http://localhost:3000/users', {
-  method: 'POST',
-  headers: headers,
-  body: jsonData
-})
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: headers,
+    body: jsonData
+  })
+  .then(response => {
+    // Handle the response
+  })
+  .then(data => {
+    // Access the converted object
+  })
+  .then(data => {
+    const id = data.id;
+    // Append the id to the DOM
+  })
+  .catch(error => {
+    // Handle the error
+  })
+  .catch(error => {
+    const errorMessage = error.message;
+    // Append the errorMessage to the DOM
+  });
+}
